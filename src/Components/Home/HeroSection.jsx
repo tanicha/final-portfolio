ddd8b9f985ab3922f36df7a939600ec287f34db6
Tani Cha <tanijcha@gmail.com>
2023-12-23 13:44:41 -0800

    feat: create hero section and css

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -3,7 +3,7 @@ export default function HeroSection() {
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section-content">
-                    <p className="section--title"> Hey, I'm Tani! </p>
+                    <p className="section--title"> Hey, I'm Tani!</p>
                     <h1 className="hero--section--title">
                         <span className="hero--section--title--color">
                             Full Stack
@@ -11,14 +11,17 @@ export default function HeroSection() {
                         <br />
                         Developer
                     </h1>
-                    <p className="hero--section-description"> Add Description Here </p>
+                    <p className="hero--section-description"> Welcome to my Personal Portfolio!</p>
                     <br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ab!
+                    I am currently working at Expedia Group as a Software Development Engineer Apprentice! Throughout this website, I will be showcasing my work experience, technical projects I've worked on, and skills/tools that I have learned to use within the last year.
                 </div>
                 <button className="btn btn-primary"> Get in touch! </button>
             </div>
             <div className="hero--section--img">
-                <img src="./img/tani-pic.jpg" alt="Hero Section"/>
+                <img className="hero--img"src="./img/tani-pic.jpg" alt="Hero Section"/>
+                <div class="overlay">
+                    <div class="text">Hello World 👾</div>
+                </div>
             </div>
         </section> 
     )
