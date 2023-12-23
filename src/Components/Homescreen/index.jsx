401ced65e34f56bed91118cfe512510156f45496
Tani Cha <tanijcha@gmail.com>
2023-12-23 15:10:39 -0800

    feat: add about me section

--- src/Components/Homescreen/index.jsx
+++ src/Components/Homescreen/index.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../Home/HeroSection";
 import Skills from "../Home/Skills"
+import AboutMe from "../Home/AboutMe";
 
 export default function Home() {
     return (
         <>
             <HeroSection />
             <Skills />
+            <AboutMe />
         </>
     );
 };
