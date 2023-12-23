7004d85698e3cf362052d08faa936b6a095ccc8c
Tani Cha <tanijcha@gmail.com>
2023-12-23 14:58:14 -0800

    feat: add skills icons and data

--- src/Components/Homescreen/index.jsx
+++ src/Components/Homescreen/index.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../Home/HeroSection";
+import Skills from "../Home/Skills"
 
 export default function Home() {
     return (
         <>
             <HeroSection />
+            <Skills />
         </>
-    )
+    );
 };
