9f26a6e8e1858009fa195bef9e049cb070cecc9f
Tani Cha <tanijcha@gmail.com>
2023-12-23 14:26:43 -0800

    feat: add navbar links to components

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -39,8 +39,85 @@ function Navbar() {
             <div>
                 <img className="logo" src="./img/logoipsum-286.svg" alt="Logo"/>
             </div>
+            <a className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav}>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+            </a>
+            <div className={`navbar--items ${navActive ? "Active" : ""}`}>
+                <ul>
+                    <li> 
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content" 
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="heroSection"
+                            className="navbar--content"
+                        >
+                        Home
+                        </Link>
+                    </li>
+                    <li> 
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content" 
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="MyPortfolio"
+                            className="navbar--content"
+                        >
+                        Portfolio
+                        </Link>
+                    </li>
+                    <li> 
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content" 
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="AboutMe"
+                            className="navbar--content"
+                        >
+                        About Me
+                        </Link>
+                    </li>
+                    <li> 
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content" 
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="Testimonials"
+                            className="navbar--content"
+                        >
+                        Testimonials
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <Link 
+                onClick={closeMenu} 
+                activeClass="navbar--active-content" 
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="Contact"
+                className="btn btn-outline-primary"
+            >
+                Contact Me
+            </Link>
         </nav>
-    )
+    );
 };
 
 export default Navbar;
