5c8b8a8a8b75f190698f8c2e82d21ac04877d6b7
Tani Cha <tanijcha@gmail.com>
2023-12-27 15:25:42 -0800

    feat: add another section & contact me section

--- src/Components/Homescreen/index.jsx
+++ src/Components/Homescreen/index.jsx
@@ -2,6 +2,8 @@ import HeroSection from "../Home/HeroSection";
 import Skills from "../Home/Skills"
 import AboutMe from "../Home/AboutMe";
 import MyPortfolio from "../Home/MyPortfolio";
+import Testimonial from "../Home/Testimonials";
+import ContactMe from "../Home/ContactMe";
 
 export default function Home() {
     return (
@@ -10,6 +12,8 @@ export default function Home() {
             <AboutMe />
             <Skills />
             <MyPortfolio />
+            <Testimonial />
+            <ContactMe />
         </>
     );
 };
