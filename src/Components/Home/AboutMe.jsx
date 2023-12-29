b227dad9818fed0c112eb4f00c9eda59fb49901b
Tani Cha <tanijcha@gmail.com>
2023-12-29 14:27:57 -0800

    feat: add content in about me section

--- src/Components/Home/AboutMe.jsx
+++ src/Components/Home/AboutMe.jsx
@@ -16,7 +16,7 @@ export default function AboutMe() {
                         My name is Tani Cha. I was born and raised in Washington, near Tacoma. I am currently working at Expedia Group, as a Software Development Engineer Apprentice in Seattle, WA.
                     </p>
                     <p className="hero--section-description">
-                        I have been coding since the start of this year (Jan 2023) - Although, I first gained some coding experience during my time in the Year Up Program (2021-2022), which was the start of my coding journey! Since the start, I have been able to make various projects using different coding languages, frameworks, libraries, etc. 
+                        I have been coding since the start of this year (Jan 2023) - Although, I first gained some coding experience during my time in the Year Up Program (2021-2022), which was the start of my coding journey! Since the beginning, with the help of many supporting coaches/peers, I have been able to make various projects using different coding languages, frameworks, libraries, etc.
                     </p>
                 </div>
             </div>
